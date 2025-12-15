5af7af76e080b5bfd70698748a3d7a76496c5ce9
Christopher Lorton <github@lortons.net>
2025-12-15 20:39:42 +0000

    🚀 prepare for release/publish

--- setup.py
+++ setup.py
@@ -66,10 +66,6 @@ def read(*names, **kwargs):
 
 
 setup(
-    # name="laser-generic",
-    # version="0.1.0",
-    # license="MIT",
-    # description="Generic disease models implented with the LASER framework.",
     long_description="{}\n{}".format(
         re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub("", read("README.md")),
         re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.md")),
@@ -82,62 +78,6 @@ setup(
     py_modules=[path.stem for path in Path("src").glob("*.py")],
     include_package_data=True,
     zip_safe=False,
-    # classifiers=[
-    #     # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    #     "Development Status :: 5 - Production/Stable",
-    #     "Intended Audience :: Developers",
-    #     "License :: OSI Approved :: MIT License",
-    #     "Operating System :: Unix",
-    #     "Operating System :: POSIX",
-    #     "Operating System :: Microsoft :: Windows",
-    #     "Programming Language :: Python",
-    #     "Programming Language :: Python :: 3",
-    #     "Programming Language :: Python :: 3 :: Only",
-    #     "Programming Language :: Python :: 3.9",
-    #     "Programming Language :: Python :: 3.10",
-    #     "Programming Language :: Python :: 3.11",
-    #     "Programming Language :: Python :: 3.12",
-    #     "Programming Language :: Python :: Implementation :: CPython",
-    #     "Programming Language :: Python :: Implementation :: PyPy",
-    #     # uncomment if you test on these interpreters:
-    #     # "Programming Language :: Python :: Implementation :: IronPython",
-    #     # "Programming Language :: Python :: Implementation :: Jython",
-    #     # "Programming Language :: Python :: Implementation :: Stackless",
-    #     "Topic :: Utilities",
-    # ],
-    # project_urls={
-    #     "Documentation": "https://institutefordiseasemodeling.github.io/laser-generic",
-    #     "Changelog": "https://institutefordiseasemodeling.github.io/laser-generic/whatsnew.html",
-    #     "Issue Tracker": "https://github.com/InstituteforDiseaseModeling/laser-generic/issues",
-    # },
-    # keywords=[
-    #     # eg: "keyword1", "keyword2", "keyword3",
-    # ],
-    # python_requires=">=3.9",
-    # install_requires=[
-    #     "click",
-    #     # eg: "aspectlib==1.1.1", "six>=1.7",
-    # ],
     extras_require={
-        # eg:
-        #   "rst": ["docutils>=0.11"],
-        #   ":python_version=='3.8'": ["backports.zoneinfo"],
     },
-    # entry_points={
-    #     "console_scripts": [
-    #         "laser-generic = laser.generic.cli:run",
-    #     ]
-    # },
-    ##    cmdclass={"build_ext": OptionalBuildExt},
-    ##    ext_modules=[
-    ##        Extension(
-    ##            str(path.relative_to("src").with_suffix("")).replace(os.sep, "."),
-    ##            sources=[str(path)],
-    ##            extra_compile_args=CFLAGS.split(),
-    ##            extra_link_args=LFLAGS.split(),
-    ##            include_dirs=[str(path.parent)],
-    ##        )
-    ##        for path in Path("src").glob("**/*.c")
-    ##    ],
-    ##    distclass=BinaryDistribution,
 )
